6654ae183b7694b8bb23dd383182ddda9417e51c
Cameron Campbell <cameronccampbell1997@gmail.com>
2025-01-16 14:07:21 +0000

    accordion block - remove bootstrap classes and replace with scss + tidy up or removing leftover attrs of removal of bootstrap accordion

--- web/app/themes/firstinternet/resources/views/blocks/accordion.blade.php
+++ web/app/themes/firstinternet/resources/views/blocks/accordion.blade.php
@@ -1,23 +1,23 @@
 <div class="{{ $block->classes }}" style="{{ $block->inlineStyle }}">
   <div class="container">
     <div class="text-center accordion-block-heading">
-      <h3 class="accordion-block-title mb-4">{{ $heading }}</h3>
+      <h3 class="accordion-block-title">{{ $heading }}</h3>
       <div>
         {!! $sub_text !!}
       </div>
     </div>
     @if (have_rows('items'))
-      <div class="accordion-fi rounded">
+      <div class="accordion-fi">
         @while (have_rows('items'))
           @php the_row() @endphp
           <div class="accordion-item">
             <h2 class="accordion-header">
-              <button class="accordion-button bg-light p-4" type="button">
+              <button class="accordion-button" type="button">
                 {{ get_sub_field('heading') }}
               </button>
             </h2>
-            <div id="collapse{{ get_row_index() }}" class="accordion-content-wrapper" data-bs-parent="#accordionExample">
-              <div class="accordion-body p-4">
+            <div id="collapse{{ get_row_index() }}" class="accordion-content-wrapper">
+              <div class="accordion-body">
                 {!! get_sub_field('text') !!}
               </div>
             </div>
